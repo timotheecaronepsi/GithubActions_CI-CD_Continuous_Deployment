1368178d836fcdf0277495b33744205a7b3a9211
Timothée CARON <timothee.caron@ecoles-epsi.net>
2026-02-04 15:22:44 +0100

    Mise à jour eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,29 @@
-import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import globals from "globals";
+import pluginJs from "@eslint/js";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+  {
+    ignores: ["**/*.test.js"],         
+    files: ["**/*.js"],           
+    languageOptions: { 
+      sourceType: "commonjs"        
+    },
+  },
+
+  {
+    languageOptions: { 
+      globals: globals.node          
+    },
+  },
+
+  pluginJs.configs.recommended,
+
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "no-console": "error",            
+    },
+  },
+];
